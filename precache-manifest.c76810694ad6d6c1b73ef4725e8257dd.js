1367b3ce1221191b320f0e300f307692dd7b4be8
Shanmuga sundar M <shanmugamaran48@gmail.com>
2019-07-21 16:46:19 +0530

    Updates

---
+++ precache-manifest.c76810694ad6d6c1b73ef4725e8257dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3775e012f9daf97e5ceaad60af0a4fc6",
+    "url": "/Todo-List/index.html"
+  },
+  {
+    "revision": "9ece6c016ddb0dfaa3cb",
+    "url": "/Todo-List/static/css/main.182f18aa.chunk.css"
+  },
+  {
+    "revision": "d2d82d68c7df91bb776f",
+    "url": "/Todo-List/static/js/2.e703e8cc.chunk.js"
+  },
+  {
+    "revision": "9ece6c016ddb0dfaa3cb",
+    "url": "/Todo-List/static/js/main.ef3da78f.chunk.js"
+  },
+  {
+    "revision": "eb313548908054f69410",
+    "url": "/Todo-List/static/js/runtime~main.9e7351aa.js"
+  }
+]);
